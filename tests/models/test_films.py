7e479344ab2b239f61859a0f52d6ced59a2fbf01
Luke Schmitt <45839909+LukeSchmitt96@users.noreply.github.com>
2023-01-10 01:35:41 -0600

    Adds pytest and some basic unit tests

---
+++ tests/models/test_films.py
@@ -0,0 +1,27 @@
+from movie_bet_bot.models.movies import Film, FilmList
+
+from ..resources.test_list import PAGE
+
+film1 = Film('film1', 'url1')
+film2 = Film('film2', 'url2')
+film3 = Film('film1', 'url1') # same as film1
+film4 = Film('film4', 'url4')
+
+class Test_Film:
+
+    def test_compare(self):
+        assert(film1 != film2)
+        assert(film1 == film3)
+
+class Test_FilmList:
+
+    def test_compare(self):
+        list1 = FilmList('url1', list([film1, film2]))
+        list2 = FilmList('url2', list([film1, film2]))
+        assert(list1 == list2)
+        list2.films.append(film4)
+        assert(list1 != list2)
+
+    def test_from_html_string(self):
+        flist = FilmList.from_html_string(PAGE)
+        assert(flist[0].title == 'Discord')
---
+++ tests/resources/test_list.py
@@ -0,0 +1,34 @@
+PAGE = """<!DOCTYPE html>
+<html id="html" lang="en" class="no-mobile no-js">
+
+<head>
+
+<body class="list-page" data-owner="LukeSchmitt">
+    <div id="content" class="site-body">
+        <div class="content-wrap">
+            <div class="cols-2">
+                <section class="section col-17 col-main overflow clearfix">
+                    <ul class="js-list-entries poster-list -p70 film-list clear film-details-list"
+                        data-remove-from-list-action="/lukeschmitt/list/test-list/remove-film/">
+                        <li class="film-detail" id="item-3405052926">
+                            <div class="really-lazy-load poster film-poster film-poster-627457 linked-film-poster"
+                                data-image-width="70" data-image-height="105" data-film-id="627457"
+                                data-film-slug="/film/discord-2020-1/" data-linked="linked"
+                                data-target-link="/film/discord-2020-1/" data-target-link-target=""
+                                data-cache-busting-key="05e0bb13" data-show-menu="true" data-hide-tooltip="true"> <img
+                                    src="https://s.ltrbxd.com/static/img/empty-poster-70.8112b435.png" class="image"
+                                    width="70" height="105" alt="Discord" /> <span class="frame"><span
+                                        class="frame-title"></span></span> </div>
+                            <div class="film-detail-content">
+                                <h2 class="headline-2 prettify"><a href="/film/discord-2020-1/">Discord</a> <small
+                                        class="metadata"><a href="/films/year/2020/">2020</a></small></h2>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>"""
