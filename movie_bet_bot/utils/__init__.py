20ea8b341ed3cda53cc4ae722bb4911aff3db235
Luke Schmitt <45839909+LukeSchmitt96@users.noreply.github.com>
2023-01-25 14:12:30 -0600

    Remove unused config module

--- movie_bet_bot/utils/__init__.py
+++ movie_bet_bot/utils/__init__.py
@@ -1,9 +1,7 @@
-from .config import parse_config
 from .fetchers import fetch_page_body_from_url
 from .utils import map_place
 
 __all__ = [
     "fetch_page_body_from_url",
     "map_place",
-    "parse_config",
 ]
