6002e360121ce55a4b0d094c5d2eae368d97fa61
Luke Schmitt <45839909+LukeSchmitt96@users.noreply.github.com>
2023-02-11 21:32:59 -0600

    Bugfix & poetry updates

--- movie_bet_bot/__main__.py
+++ movie_bet_bot/__main__.py
@@ -2,7 +2,7 @@ import yaml
 from dotenv import load_dotenv
 
 from movie_bet_bot.models.client import MovieBetBot
-from movie_bet_bot.models.movies import Contest
+from movie_bet_bot.models.movies import movies
 from movie_bet_bot.utils import constants
 
 load_dotenv()
@@ -13,7 +13,7 @@ def main():
     with open(constants.DB_PATH) as conf_file:
         print("Opened db!")
         saved_data = yaml.safe_load(conf_file)
-        bot = MovieBetBot(contest=Contest.from_config(saved_data["contests"])[0])
+        bot = MovieBetBot(contest=movies.Contest.from_config(saved_data["contests"])[0])
     bot.run()
 
 
