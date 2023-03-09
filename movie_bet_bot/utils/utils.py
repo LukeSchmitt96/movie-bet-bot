e35c0c5a3cc6cb20e5c4ac985102899e6d48dfb7
Luke Schmitt <45839909+LukeSchmitt96@users.noreply.github.com>
2023-03-09 01:30:43 -0600

    Clean up utils.py
    Add missing docstring.

--- movie_bet_bot/utils/utils.py
+++ movie_bet_bot/utils/utils.py
@@ -1,4 +1,10 @@
 def map_place(place: int) -> str:
+    """
+    Map place to corresponding emoji or ordinal number.
+
+    :param place: place to map
+    :return: emoji or ordinal number representing place in standings
+    """
     match place:
         case 1:
             return "🥇"
